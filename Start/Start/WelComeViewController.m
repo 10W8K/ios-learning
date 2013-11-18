35d90bb0954c46d6bf6a487593ed9680d4c02032
想当当 <donghua.yan@alipay.com>
2013-11-18 15:48:15 +0800

    update

--- Start/Start/WelComeViewController.m
+++ Start/Start/WelComeViewController.m
@@ -29,19 +29,19 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
     
-    self.scrollView = [[UIScrollView alloc] initWithFrame:CGRectMake(0, 0, 340, 460)];
+    self.scrollView = [[UIScrollView alloc] initWithFrame:CGRectMake(0, 0, 340, 760)];
     self.scrollView.backgroundColor = [UIColor whiteColor];
     self.scrollView.delegate = self;
     self.scrollView.pagingEnabled = true;
     self.scrollView.tag = INT_MAX;
     self.scrollView.showsHorizontalScrollIndicator = NO;
     
-    self.scrollView.contentSize = CGSizeMake(340 * 4, 460);
+    self.scrollView.contentSize = CGSizeMake(340 * 4, 480);
     [self.view addSubview:self.scrollView];
     
     int _x = 0;
     for (int index = 0; index < 4; index++){
-        UIImageView *imgScrollView = [[UIImageView alloc] initWithFrame:CGRectMake(0+_x, 0, 320, 460)];
+        UIImageView *imgScrollView = [[UIImageView alloc] initWithFrame:CGRectMake(0+_x, 0, 320, 510)];
         imgScrollView.tag = index;
         NSString *imgName = [NSString stringWithFormat:@"%d.png", index + 1];
         imgScrollView.image = [UIImage imageNamed:imgName];
@@ -51,26 +51,25 @@
     
     
     
-    UIPageControl *pageControl = [[UIPageControl alloc] initWithFrame:CGRectMake(0, 380, 320, 50)];
+    UIPageControl *pageControl = [[UIPageControl alloc] initWithFrame:CGRectMake(0, 460, 320, 50)];
     pageControl.numberOfPages = 4;
     pageControl.tag = 101;
-    //pageControl.backgroundColor = [UIColor colorWithHex:0xff0000];
     [self.view addSubview:pageControl];
     
     //UIImage *img = [UIImage imageNamed:@"Default.png"];
     //[self.view setBackgroundColor:[UIColor colorWithPatternImage:img]];
     
-    /**
+    /***
     [pageControl setTranslatesAutoresizingMaskIntoConstraints:NO];
-    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-0-[pageControl(==50)]-20-|"
+    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-0-[pageControl]-100-|"
                                                                       options:0
                                                                       metrics:nil
                                                                         views:NSDictionaryOfVariableBindings(pageControl)]];
-    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-0-[pageControl]-0-|"
+    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-100-[pageControl]-100-|"
                                                                       options:0
                                                                       metrics:nil
                                                                         views:NSDictionaryOfVariableBindings(pageControl)]];
-     */
+     **/
     
 }
 
